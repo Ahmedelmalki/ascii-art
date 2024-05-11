dd60bcb1ff039a849a0153ad00b7af304c9ac319
aelmalki <elmalkiahmed02@gmail.com>
2024-05-11 15:13:02 +0100

    ascii-art.11.may

--- main/main.go
+++ main/main.go
@@ -7,38 +7,42 @@ import (
 )
 
 func main() {
-	arr := os.Args[1]
-	for _, char := range arr {
-		if char > 126 || char < 32 {
-			fmt.Println("invalid charactere")
+	if len(os.Args) != 2 {
+		fmt.Println("tiaaaaaaaaakh")
+	} else {
+		arr := os.Args[1]
+		for _, char := range arr {
+			if char > 126 || char < 32 {
+				fmt.Println("invalid charactere")
+				return
+			}
+		}
+		file, err := os.ReadFile("standard.txt")
+		if err != nil {
+			fmt.Println(err)
 			return
 		}
-	}
-	file, err := os.ReadFile("standard.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	filesplit := strings.Split(string(file), "\n")
-	arrsplit := strings.Split(arr, "\\n")
-	if len(arrsplit) >= 2 && arrsplit[0] == "" && arrsplit[1] == "" {
-		if len(arrsplit) == 2 {
-			fmt.Println()
-		}
-		arrsplit = arrsplit[1:]
-	}
-	for _, word := range arrsplit {
-		if word == "" && len(arrsplit) == 1 {
-			continue
-		} else if word == "" {
-			fmt.Println()
-			continue
-		}
-		for j := 1; j < 9; j++ {
-			for i := 0; i < len(word); i++ {
-				fmt.Print(filesplit[(int(word[i])-32)*9+j])
+		filesplit := strings.Split(string(file), "\n")
+		arrsplit := strings.Split(arr, "\\n")
+		isprinted := false
+		for index, word := range arrsplit {
+			if word != "" {
+				for j := 1; j < 9; j++ {
+					for i := 0; i < len(word); i++ {
+						fmt.Print(filesplit[(int(word[i])-32)*9+j])
+						isprinted = true
+					}
+					fmt.Println("y")
+				}
+			} else if word == "" {
+				if index == len(arrsplit)-1 && !isprinted {
+					continue
+				} else {
+					fmt.Println("x")
+					continue
+				}
 			}
-			fmt.Println()
 		}
 	}
+
 }
